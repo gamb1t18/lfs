cd2b0874faf52c9ac8e24c01a4354f45def01a2f
gamb1t18 <122176429+gamb1t18@users.noreply.github.com>
2024-12-08 17:53:51 +0000

    Update 7b_pkgs.sh

--- 7b_pkgs.sh
+++ 7b_pkgs.sh
@@ -482,12 +482,12 @@ mkdir -pv /usr/share/fonts/unifont &&
 gunzip -c ../unifont-15.1.05.pcf.gz > /usr/share/fonts/unifont/unifont.pcf
 echo depends bli part_gpt > grub-core/extra_deps.lst
 ./configure --prefix=/usr        \
-            --sysconfdir=/etc    \
-            --disable-efiemu     \
-            --enable-grub-mkfont \
-            --with-platform=efi  \
-            --target=x86_64      \
-            --disable-werror     &&
+--sysconfdir=/etc    \
+--disable-efiemu     \
+--enable-grub-mkfont \
+--with-platform=efi  \
+--target=x86_64      \
+--disable-werror     &&
 unset TARGET_CC &&
 make
 make install &&
